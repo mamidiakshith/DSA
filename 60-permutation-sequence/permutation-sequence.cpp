355a90784873c8513ea1937e889bbb4d7c211d0f
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-03-31 23:28:43 +0530

    Time: 52 ms (19.52%) | Memory: 8.5 MB (74.18%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.cpp
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     string getPermutation(int n, int k) {
         string s="";
-        vector<string>v;
         for(int i=1;i<=n;i++){
             s+=to_string(i);
         }
         sort(s.begin(),s.end());
-        v.push_back(s);
-        for(int i=0;i<k;i++){
+        for(int i=1;i<k;i++){
             next_permutation(s.begin(),s.end());
-            v.push_back(s);
         }
-        return v[k-1];
+        return s;
             }
 };
