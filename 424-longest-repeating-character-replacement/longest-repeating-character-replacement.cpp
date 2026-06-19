fe735acb9ba29fb99ae4321fe714af832f036583
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-06-19 11:36:19 +0530

    Time: 3 ms (68.62%) | Memory: 10.8 MB (92.85%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int l=0,r=0,ml=0,mf=0;
+        int r=0,l=0,ml=0,mf=0;
         vector<int>v(26,0);
         while(r<s.size()){
-            v[s[r]-'A']++;
-            mf=max(mf,v[s[r]-'A']);
-            while( (r-l+1)-mf >k){
-                v[s[l]-'A']--;
-                l++;
-            }
-            ml=max(ml,r-l+1);
-            r++;
+        v[s[r]-'A']++;
+        mf=max(mf,v[s[r]-'A']);
+        while((r-l+1)-mf >k){
+            v[s[l]-'A']--;
+            l++;
         }
-        return ml;
+        ml=max(ml,r-l+1);
+        r++;}
+            return ml;
     }
 };
