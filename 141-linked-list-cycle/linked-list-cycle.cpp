0bfb14080d65031ca260276ace16830b3c60aea1
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-06-22 12:37:26 +0530

    Time: 12 ms (43.55%) | Memory: 11.8 MB (80.94%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -15,8 +15,9 @@ public:
         while(fast!=nullptr && fast->next!=nullptr){
             slow=slow->next;
             fast=fast->next->next;
-            if(fast==slow){
-                return true;
+            if(slow==fast){
+                flag=true;
+                break;
             }
         }
         return flag;
