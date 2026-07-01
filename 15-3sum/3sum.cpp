f7f749f445e2bc6b3051e67a194610457b8be986
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-01 12:01:17 +0530

    Time: 54 ms (31.04%) | Memory: 31.1 MB (17.58%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,7 +6,7 @@ public:
         int i=0;
         sort(nums.begin(),nums.end());
         while(i<nums.size()){
-            if (i > 0 && nums[i] == nums[i - 1]){
+            if(i>0 && nums[i]==nums[i-1]){
                 i++;
                 continue;
             }
@@ -14,19 +14,19 @@ public:
             int k=nums.size()-1;
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
-                if(sum>0){
-                    k--;
-                }
-                else if(sum<0){
+                if(sum<0){
                     j++;
+                }
+                else if(sum>0){
+                    k--;
                 }
                 else{
-                    vector<int>temp={nums[i],nums[j],nums[k]};
-                    while(j<k && nums[j]==nums[j+1])j++;
-                    while(j<k && nums[k]==nums[k-1])k--;
-                    res.push_back(temp);
+                    vector<int> lvl ={nums[i],nums[j],nums[k]};
                     j++;
                     k--;
+                    while(j<k && nums[j]==nums[j-1])j++;
+                    while(j<k && nums[k]==nums[k+1])k--;
+                    res.push_back(lvl);
                 }
             }
             i++;
