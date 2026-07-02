b2b250c25fec1f0ada90816522390dbe7543188e
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-02 13:25:10 +0530

    Time: 8 ms (55.01%) | Memory: 11.5 MB (43.32%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int r=0,l=0,c=0,si=-1;
-        int minlen=INT_MAX;
         unordered_map<char,int>m;
         for(int i=0;i<t.size();i++){
             m[t[i]]++;
         }
+        int r=0,l=0,si=-1;
+        int c=0;
+        int minlen=INT_MAX;
         while(r<s.size()){
             if(m[s[r]]>0){
-                c++;}
-                m[s[r]]--;
+                c++;
+            }
+            m[s[r]]--;
             while(c==t.size()){
-                if( r-l+1 <minlen){
+                if( r-l+1 < minlen ){
                     minlen=r-l+1;
                     si=l;
                 }
                 m[s[l]]++;
-                if(m[s[l]]>0){
-                    c--;
-                }
+                if(m[s[l]]>0)c--;
                 l++;
             }
             r++;
         }
-        if(si==-1){
-            return "";
-        }
-        return s.substr(si,minlen);
+        return si==-1?"":s.substr(si,minlen);
     }
 };
