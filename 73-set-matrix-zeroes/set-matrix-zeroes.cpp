0377c285d55a84fc32c1867acd075a6fb3ad159a
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-03 20:30:53 +0530

    Time: 29 ms (9.61%) | Memory: 20.8 MB (76.76%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    void rowzero(int x, int cols, vector<vector<int>>& matrix){
-        for(int j = 0; j < cols; j++){
-            if(matrix[x][j] != 0) matrix[x][j] = -999;
+
+    void rowzero(int x,int rows,vector<vector<int>>& matrix){
+        for(int i=0;i<rows;i++){
+            if(matrix[x][i]!=0)matrix[x][i]=-999;
         }
     }
-    void colzero(int x, int rows, vector<vector<int>>& matrix){
-        for(int i = 0; i < rows; i++){
-            if(matrix[i][x] != 0) matrix[i][x] = -999;
+    void colzero(int x,int cols,vector<vector<int>>& matrix){
+        for(int i=0;i<cols;i++){
+            if(matrix[i][x]!=0)matrix[i][x]=-999;
         }
     }
     void setZeroes(vector<vector<int>>& matrix) {
-        int rows = matrix.size();               // number of rows
-        int cols = matrix[0].size();            // number of columns (in the first row)
+        int rows=matrix.size();
+        int cols=matrix[0].size();
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
                 if(matrix[i][j]==0){
@@ -21,11 +22,13 @@ public:
                 }
             }
         }
-        for(int i = 0; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                if(matrix[i][j] == -999)
-                    matrix[i][j] = 0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]==-999){
+                    matrix[i][j]=0;
+                }
             }
         }
+
     }
 };
