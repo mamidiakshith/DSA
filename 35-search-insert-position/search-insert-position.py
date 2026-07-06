8c78f3f61ee7254b182fed6d057598bd8c996d97
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-06 08:32:53 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (11.06%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l=0
+        r=len(nums)-1
+        ans=len(nums)
+        while l<=r:
+            mid= l+ (r-l)//2
+            if nums[mid]>=target:
+                ans=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return ans
+
+        
