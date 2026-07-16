f85c1b4f67554dc6a637cb1eb4dbc889d8305533
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-16 20:43:19 +0530

    Time: 161 ms (81.16%) | Memory: 20.6 MB (79.95%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -4,21 +4,15 @@ class Solution:
         r=max(piles)
         ans=float('inf')
         while l<=r:
-            mid= l+ (r-l)//2
-            k=mid
+            mid= l + (r-l)//2
             t=0
             for i in piles:
-                t+= (i+k-1)//k
+                t+= (i+mid-1)//mid
             if t<=h:
-                ans=min(ans,k)
+                ans=min(ans,mid)
                 r=mid-1
             else:
                 l=mid+1
         return ans
             
-            
-
-            
-
-
             
