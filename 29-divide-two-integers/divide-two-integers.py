6853dfb01df187ebe811c4d613b7ba51d2d2a7be
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-20 11:37:40 +0530

    Time: 4 ms (6.30%) | Memory: 19 MB (98.90%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.py
+++ 29-divide-two-integers/divide-two-integers.py
@@ -1,6 +1,5 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        if dividend == -2**31 and divisor == -1:
-            return 2**31 - 1  # INT_MAX
-        return int(dividend / divisor)
-        
+        if dividend== -2**31 and divisor==-1:
+            return 2**31-1
+        return int(dividend/divisor)
