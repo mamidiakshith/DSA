1c4838a9a6356e166912e110fd11b1364d10d569
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-23 15:10:41 +0530

    Time: 23 ms (16.52%) | Memory: 19.5 MB (27.85%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,16 +1,12 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         a=[]
-        for i in range(len(nums1)):
-            flag=-1
-            x=nums2.index(nums1[i])
-            for j in range(x+1,len(nums2)):
-                if nums2[j]>nums1[i]:
-                    flag=nums2[j]
+        for i in nums1:
+            idx=nums2.index(i)
+            found=-1
+            for j in range(idx,len(nums2)):
+                if nums2[j]>i:
+                    found=nums2[j]
                     break
-            a.append(flag)
+            a.append(found)
         return a
-        
-            
-
-
