304b2d14f63a41e88a598d778f25e4343f9779e1
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-24 11:29:20 +0530

    Time: 7 ms (61.02%) | Memory: 11.6 MB (26.55%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -5,25 +5,28 @@ public:
         for(int i=0;i<t.size();i++){
             m[t[i]]++;
         }
-        int r=0,l=0,si=-1;
+        int ml=INT_MAX,si=-1;
         int c=0;
-        int minlen=INT_MAX;
+        int r=0,l=0;
         while(r<s.size()){
             if(m[s[r]]>0){
                 c++;
             }
             m[s[r]]--;
             while(c==t.size()){
-                if( r-l+1 < minlen ){
-                    minlen=r-l+1;
+                if((r-l+1)<ml){
+                    ml= r-l+1;
                     si=l;
                 }
                 m[s[l]]++;
-                if(m[s[l]]>0)c--;
+                if(m[s[l]]>0){
+                    c--;
+                }
                 l++;
             }
             r++;
         }
-        return si==-1?"":s.substr(si,minlen);
+        return si==-1?"":s.substr(si,ml);
     }
 };
+
