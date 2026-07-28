973c7c12afa1b55730492ac488ea3b65b2ee629d
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-28 19:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.39%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -3,13 +3,11 @@ public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
         for(int i=0;i<n;i++){
-            int mini=i;
             for(int j=i+1;j<n;j++){
-                if(nums[mini]>nums[j]){
-                    mini=j;
+                if(nums[j]<nums[i]){
+                    swap(nums[j],nums[i]);
                 }
             }
-            swap(nums[mini],nums[i]);
         }
     }
 };
