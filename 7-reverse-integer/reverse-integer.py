3df2997611b3bf2f31c6753e7addfbad114d7b32
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-07-31 11:17:11 +0530

    Time: 50 ms (31.64%) | Memory: 19.2 MB (69.50%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -5,14 +5,11 @@ class Solution:
         flag =1
         if x<0:
             flag=-1
-        k=abs(x)
-        while(k!=0):
-            r= k%10
-            s=s*10+r
-            k=k//10
-        s *=flag
-        if s< -2**31 or s> 2**31-1:
+        k=str(abs(x))[::-1]
+        ans= int(k)*flag
+        if ans< -2**31 or ans> 2**31-1:
             return 0
-        return s
+        return ans
+        
 
         
