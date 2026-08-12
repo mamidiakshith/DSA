267c3589a2103dd0178b0b0701bbcecc0c096206
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-08-12 11:38:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (23.32%) - LeetSync

--- 342-power-of-four/power-of-four.py
+++ 342-power-of-four/power-of-four.py
@@ -3,9 +3,6 @@ class Solution:
         if n<=0:
             return False
         c=0
-        while n>1:
-            if n & 1:
-                return False
-            n>>=1
-            c+=1
-        return c%2==0
+        while n%4==0:
+            n//=4
+        return n==1
