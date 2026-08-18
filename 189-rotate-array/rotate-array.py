8f061974db2d3be18b3ca157c4a1a25ab2214014
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-08-18 18:49:27 +0530

    Time: 60 ms (13.47%) | Memory: 35 MB (7.10%) - LeetSync

---
+++ 189-rotate-array/rotate-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def rotate(self, nums: list[int], k: int) -> None:
+        n=len(nums)
+        k=k%n
+        nums.reverse()
+        nums[k:]=reversed(nums[k:])
+        nums[:k]=reversed(nums[:k])
