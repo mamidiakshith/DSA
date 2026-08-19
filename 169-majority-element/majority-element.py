9dd4cd6df912c9756aa6ab9cbe4b0e92fa6bfcce
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-08-19 11:36:27 +0530

    Time: 9 ms (37.13%) | Memory: 21.2 MB (46.89%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,9 +1,11 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         m={}
+        n=len(nums)
         for i in nums:
             m[i]=m.get(i,0)+1
         for i,j in m.items():
-            if j> len(nums)//2:
+            if j>n//2:
                 return i
         return -1
+        
