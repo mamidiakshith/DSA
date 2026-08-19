13b5e2932e54a49a552693853efde01eaf7f51e0
Akshith Reddy Mamidi <151131099+mamidiakshith@users.noreply.github.com>
2026-08-19 11:33:59 +0530

    Time: 4 ms (4.55%) | Memory: 19.2 MB (90.62%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -4,6 +4,10 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         for i in range(len(nums)):
-            for j in range(i,len(nums)):
-                if nums[i]>nums[j]:
-                    nums[i],nums[j]=nums[j],nums[i]
+            mini=i
+            flag=False
+            for j in range(i+1,len(nums)):
+                if nums[j]<nums[mini]:
+                    mini=j
+            nums[i],nums[mini]=nums[mini],nums[i]
+                
